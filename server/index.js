b68380ee78a96500c0ed6f5af8089a12ca950b18
Anthony Loredo <anthonyloredo5@gmail.com>
2021-02-16 00:31:48 -0500

    added env file with hidden variables and example env file

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 
 //middle for mongoose
@@ -17,11 +19,10 @@ app.use(cors());
 //adds "posts" to all post routes coming into this file
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://anthonyloredo5:anthonyloredo5123@cluster0.dtlwh.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 //db connection
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
 
